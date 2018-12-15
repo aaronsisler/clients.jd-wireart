6e5d36f64060d82a51dc5bc17ffb37db3e0c2b2d
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2018-12-15 11:24:16 -0500

    Fixing ESLint and babel plugin issues.  Adding in loading screen logic for gallery container

--- src/components/core/LandingPage.js
+++ src/components/core/LandingPage.js
@@ -12,13 +12,13 @@ const LandingPage = () => (
             <div className="landing_page_content__spacer" />
             <div className="landing_page_content__gallery">
                 <Link
-                    className="landing_page_content__nav_link-action"
+                    className="landing_page_content__nav_link action"
                     to="/gallery"
                 >
-                    Browse our collection
+                    Browse our gallery
             </Link>
                 <Link
-                    className="landing_page_content__nav_link-secondary"
+                    className="landing_page_content__nav_link secondary"
                     to="/about"
                 >
                     Learn more about us
--- src/components/gallery/GalleryContainer.js
+++ src/components/gallery/GalleryContainer.js
@@ -6,17 +6,31 @@ import { startSetGallery } from 'Actions/gallery';
 
 class GalleryContainer extends React.Component {
     constructor(props) {
-        super(props)
-        props.startSetGallery();
+        super(props);
+        this.state = {
+            isLoading: true,
+        }
+    }
+
+    componentDidMount() {
+        this.handleRetrieveGallery();
+    }
+
+    handleRetrieveGallery = async () => {
+        await this.props.startSetGallery();
+        this.setState(() => ({ isLoading: false }));
     }
 
     render() {
+        const { gallery } = this.props;
         return (
             <div className="gallery_container">
-                <h2>
-                    Gallery Container
-                </h2>
-                <GalleryList gallery={this.props.gallery} />
+                <div className="gallery_container__title">
+                    Gallery
+                </div>
+                {this.state.isLoading
+                    && <div className="gallery_container__loading">Getting our lastest pieces</div>}
+                {!this.state.isLoading && <GalleryList gallery={gallery} />}
             </div>
         )
     }
