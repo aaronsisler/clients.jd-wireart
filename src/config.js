5b6467228c22d206fabf638ac8c8f7d107637516
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-01-03 13:52:29 -0500

    Removing the secret

--- src/config.js
+++ src/config.js
@@ -4,4 +4,3 @@ export const apiGatewayToken = 'gVs4ABqLyE49xdzgqqNataz0BD6wwHVA5c5AWv1R';
 export const pointOfContactEmail = 'aaron.sisler@eandbsolutions.com';
 export const websiteURL = "http://jdwireart.firebaseapp.com";
 export const paypalSandboxClientID = "AXItpK63JSFjy_PXAWvgrg7zpjexyvqhJbbyHq8yBliyR551HcfRGgWIjOuYH3tQJ9HzgaVHx5RtY91Y";
-export const paypalSandboxSecret = "EEiT45Im_-CJXBHjloVWv8wkIwmtl2mwPmjlssFzB57XDuuVjzN4RnlZ4Crb8Snz4ZgaqTBxJ6wBjo8E";
