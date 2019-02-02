29629755717ead6fdade8562328aa1711ba3e113
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-02-01 21:18:11 -0500

    Adding dimensions is working.  Need to display to customer now

--- src/components/gallery/GalleryPieceForm.js
+++ src/components/gallery/GalleryPieceForm.js
@@ -9,6 +9,9 @@ export default class GalleryPieceForm extends React.Component {
         this.state = {
             name: props.galleryPiece ? props.galleryPiece.name : '',
             price: props.galleryPiece ? convertPrice(props.galleryPiece.price) : '',
+            length: props.galleryPiece ? props.galleryPiece.length : '',
+            width: props.galleryPiece ? props.galleryPiece.width : '',
+            height: props.galleryPiece ? props.galleryPiece.height : '',
             error: '',
         };
     }
@@ -19,31 +22,44 @@ export default class GalleryPieceForm extends React.Component {
             this.setState(() => ({ error: 'Please provide name and price' }));
         } else {
             this.setState(() => ({ error: '' }));
+            const { error, ...submitObject } = Object.assign({}, this.state); // eslint-disable-line no-unused-vars
+
             this.props.onSubmit({
-                name,
+                ...submitObject,
                 price: convertPrice(price, false),
             })
         }
     }
 
+    onDimensionChange = (e) => {
+        const { name: inputName, value: inputValue } = e.target;
+
+        if (!inputValue || inputValue.match(/^[0-9]*$/)) {
+            return this.setState({ [inputName]: inputValue });
+        }
+
+        return this.setState({ [inputName]: '' });
+    }
+
     onNameChange = (e) => {
         const name = e.target.value;
-        this.setState(() => ({ name }));
+        return this.setState(() => ({ name }));
     }
 
     onPriceChange = (e) => {
         const price = e.target.value;
 
         if (!price || price.match(/^\d{1,}(\.\d{0,2})?$/)) {
-            this.setState(() => ({ price }));
+            return this.setState(() => ({ price }));
         }
     }
 
     render() {
-        const { error, name, price } = this.state;
+        const { error, height, length, name, price, width } = this.state;
         return (
             <div className="gallery_piece_form">
                 {error && <FormError error={error} />}
+                <div>Name</div>
                 <input
                     className="gallery_piece_form__input"
                     onChange={this.onNameChange}
@@ -51,6 +67,7 @@ export default class GalleryPieceForm extends React.Component {
                     type="text"
                     value={name}
                 />
+                <div>Price</div>
                 <input
                     className="gallery_piece_form__input"
                     onChange={this.onPriceChange}
@@ -58,6 +75,38 @@ export default class GalleryPieceForm extends React.Component {
                     type="text"
                     value={price}
                 />
+                <div className="gallery_piece_form__dimensions">
+                    <div>Length&nbsp;(in.)
+                    <input
+                            className="gallery_piece_form__input"
+                            name="length"
+                            onChange={this.onDimensionChange}
+                            placeholder="Length"
+                            type="text"
+                            value={length}
+                        />
+                    </div>
+                    <div>Width&nbsp;(in.)
+                    <input
+                            className="gallery_piece_form__input"
+                            name="width"
+                            onChange={this.onDimensionChange}
+                            placeholder="Width"
+                            type="text"
+                            value={width}
+                        />
+                    </div>
+                    <div>Height&nbsp;(in.)
+                    <input
+                            className="gallery_piece_form__input"
+                            name="height"
+                            onChange={this.onDimensionChange}
+                            placeholder="Height"
+                            type="text"
+                            value={height}
+                        />
+                    </div>
+                </div>
                 <button
                     className="nav_link gallery_piece_form__button"
                     onClick={this.handleSubmit}
