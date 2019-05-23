a83e7a2f8e6c5143dd4b0f3650d47b07eb2bb044
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-05-23 09:44:36 -0400

    Ripping out the old files

--- src/components/core/NotFoundPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NotFoundPage = () => (
-    <div>
-        404! This is not the page you are looking for!
-    </div>
-);
-
-export default NotFoundPage;
--- src/selectors/gallery.js
+++
@@ -1,3 +0,0 @@
-// Get filtered gallery pieces by name
-export default (gallery, { text = '' }) => gallery
-    .filter((galleryPiece) => galleryPiece.name.toLowerCase().includes(text.toLowerCase()))
