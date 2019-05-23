6380d16221ea4cbe60e211f6c7c0c1a0ccc97f7c
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-05-23 10:04:21 -0400

    Porting over valleylovefarm CRA, moving secrets, switching most non-component things to JDWireArt

---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+// Email
+export const pointOfContactEmail = "";
+
+//Services Gateway
+export const servicesApiGatewayURL =
+  "https://2des5sl17b.execute-api.us-east-1.amazonaws.com/prod/send-email";
---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+import { pointOfContactEmail, servicesApiGatewayURL } from "./config";
+
+import { servicesApiGatewayToken } from "./secrets";
+
+export { pointOfContactEmail, servicesApiGatewayURL, servicesApiGatewayToken };
