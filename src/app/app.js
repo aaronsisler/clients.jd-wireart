44b6d99d7f3ee9d6f565846a425492ba8c5eaeac
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-05-23 10:16:39 -0400

    Updating the fonts and some colors in the varibales file

--- src/app/app.js
+++ src/app/app.js
@@ -3,9 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import AppContainer from "../containers/app-container";
 import "normalize.css/normalize.css";
 import "./app.scss";
-import { initializeAnalytics } from "../tools/analytics";
-
-initializeAnalytics();
 
 const App = () => (
   <BrowserRouter>
