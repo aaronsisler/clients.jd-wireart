4cfd831d308042694647df029417c3756ac932bf
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-06-06 14:21:23 -0400

    Starting to populate the manager details to utilize mobile views

--- src/components/manager-details/manager-details.js
+++ src/components/manager-details/manager-details.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import "./manager-details.scss";
 
-const ManagerDetails = () => (
-  <div className="manager-details inbox__details">ManagerDetails</div>
-);
+const ManagerDetails = ({ galleryPiece }) => {
+  if (!galleryPiece) {
+    return (
+      <div className="manager-details inbox__details-empty inbox-mobile">
+        Please select an item to view
+      </div>
+    );
+  }
+  return (
+    <div className="manager-details inbox__details">
+      <Link className="nav-link">Back to list</Link>
+      ManagerDetails
+    </div>
+  );
+};
+
+ManagerDetails.propTypes = {
+  galleryPiece: PropTypes.object
+};
 
 export default ManagerDetails;
