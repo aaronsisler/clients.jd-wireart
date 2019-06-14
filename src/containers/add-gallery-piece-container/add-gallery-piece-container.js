96d9d3e3ce278222faf6c5c5eb99359a32caaf35
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-06-14 15:36:17 -0400

    Working through the add gallery piece.  Need to desktop CSS and find a way to retrieve ID to push back to manager/id page

--- src/containers/add-gallery-piece-container/add-gallery-piece-container.js
+++ src/containers/add-gallery-piece-container/add-gallery-piece-container.js
@@ -1,8 +1,41 @@
 import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import GalleryPieceForm from "../../components/gallery-piece-form";
+import { startAddGalleryPiece } from "../../actions";
+import history from "../../tools/history";
 import "./add-gallery-piece-container.scss";
 
-const AddGalleryPieceContainer = () => (
-  <div className="add-gallery-piece-container">AddGalleryPieceContainer</div>
-);
+class AddGalleryPieceContainer extends React.Component {
+  handleSubmit = galleryPiece => {
+    this.props.startAddGalleryPiece(galleryPiece);
+    return history.push("/manager");
+  };
 
-export default AddGalleryPieceContainer;
+  render() {
+    return (
+      <div className="add-gallery-piece-container">
+        <div className="add-gallery-piece-container__form">
+          <div className="add-gallery-piece-container__form-title">
+            Add Gallery Piece
+          </div>
+          <GalleryPieceForm onSubmit={this.handleSubmit} />
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  startAddGalleryPiece: galleryPiece =>
+    dispatch(startAddGalleryPiece(galleryPiece))
+});
+
+AddGalleryPieceContainer.propTypes = {
+  startAddGalleryPiece: PropTypes.func.isRequired
+};
+
+export default connect(
+  undefined,
+  mapDispatchToProps
+)(AddGalleryPieceContainer);
