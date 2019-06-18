5fb85099ca053b42778ad99e69da5749eb763952
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-06-18 12:24:51 -0400

    Adding in the loading spinner for when app first is loading

--- src/containers/loading-container/loading-container.js
+++ src/containers/loading-container/loading-container.js
@@ -1,6 +1,14 @@
 import React from "react";
+import LoadingSpinner from "../../components/loading-spinner";
 import "./loading-container.scss";
 
-const LoadingContainer = () => <div className="loading-container">LoadingContainer</div>;
+const LoadingContainer = () => (
+  <div className="loading-container">
+    <div className="loading-container__text">Loading</div>
+    <div className="loading-container__spinner">
+      <LoadingSpinner />
+    </div>
+  </div>
+);
 
 export default LoadingContainer;
