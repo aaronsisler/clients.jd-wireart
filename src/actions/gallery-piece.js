aed8de07a13f4c652d69130461a692c609641d35
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-06-26 16:06:03 -0400

    Working through the edit GP notification. Adjusting action calls to be async.  Need to test

--- src/actions/gallery-piece.js
+++ src/actions/gallery-piece.js
@@ -21,35 +21,38 @@ export const startAddGalleryPiece = galleryPiece => async dispatch => {
 export const startAddGalleryPieceImage = (
   galleryPieceId,
   imageSrc
-) => dispatch =>
-  database
-    .ref(`gallery/${galleryPieceId}`)
-    .update({ imageSrc })
-    .then(() => dispatch(editGalleryPiece(galleryPieceId, { imageSrc })));
+) => async dispatch => {
+  await database.ref(`gallery/${galleryPieceId}`).update({ imageSrc });
 
-export const startEditGalleryPiece = (galleryPieceId, updates) => dispatch =>
-  database
-    .ref(`gallery/${galleryPieceId}`)
-    .update(updates)
-    .then(() => dispatch(editGalleryPiece(galleryPieceId, updates)));
+  return dispatch(editGalleryPiece(galleryPieceId, { imageSrc }));
+};
 
-export const startSetPaymentActiveFlag = galleryPieceId => dispatch =>
-  database
-    .ref(`gallery/${galleryPieceId}`)
-    .update({ isPaymentActive: true })
-    .then(() =>
-      dispatch(editGalleryPiece(galleryPieceId, { isPaymentActive: true }))
-    );
+export const startEditGalleryPiece = (
+  galleryPieceId,
+  updates
+) => async dispatch => {
+  await database.ref(`gallery/${galleryPieceId}`).update(updates);
 
-export const startMarkAsSold = galleryPieceId => dispatch =>
-  database
+  return dispatch(editGalleryPiece(galleryPieceId, updates));
+};
+
+export const startSetPaymentActiveFlag = galleryPieceId => async dispatch => {
+  await database
     .ref(`gallery/${galleryPieceId}`)
-    .update({ isPaymentActive: false, isSold: true })
-    .then(() =>
-      dispatch(
-        editGalleryPiece(galleryPieceId, {
-          isPaymentActive: false,
-          isSold: true
-        })
-      )
-    );
+    .update({ isPaymentActive: true });
+
+  return dispatch(editGalleryPiece(galleryPieceId, { isPaymentActive: true }));
+};
+
+export const startMarkAsSold = galleryPieceId => async dispatch => {
+  await database
+    .ref(`gallery/${galleryPieceId}`)
+    .update({ isPaymentActive: false, isSold: true });
+
+  return dispatch(
+    editGalleryPiece(galleryPieceId, {
+      isPaymentActive: false,
+      isSold: true
+    })
+  );
+};
