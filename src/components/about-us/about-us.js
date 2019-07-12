b423ff836ec5cfd49fe721bf3c661458da573d64
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-07-12 10:13:45 -0400

    Adjusting the text and CSS for About Us

--- src/components/about-us/about-us.js
+++ src/components/about-us/about-us.js
@@ -3,25 +3,55 @@ import "./about-us.scss";
 
 const AboutUs = () => (
   <div className="about-us">
-    <div className="about-us__title">About us</div>
-    <div className="about-us__content">
-      <div className="about-us__content-image">
-        <img src="images/about/about.jpg" alt="about us" />
-      </div>
-      <div className="about-us__content-text">
-        <div>
-          <strong>Valley Love Farm</strong> was started to bring high quality
-          produce to local customers. We work hard to maintain sustainable and
-          environmentally friendly practices by allowing our chickens to range
-          free. This provides benefits to the environment and produces a
-          healthier chicken, which in turn provides a healthier you!
-        </div>
-        <div>
-          <strong>Community Supported Agriculture (CSA)</strong> is the perfect
-          way for a customer, who cares about local food, to ally with a farmer
-          and get the best they have to offer.
-        </div>
-      </div>
+    <div className="about-us__title">About Me</div>
+    <div className="about-us__text">
+      My name is Jonathan Daniel. I was born in 1965 on a farm in Zimbabwe, and
+      I am the oldest of nine children. Because my parents were so poor, I spent
+      my childhood with no clothes, money, or toys.
+    </div>
+    <div className="about-us__text">
+      Although we were materially poor, my family was rich in values and
+      tradition. Poverty forces you to be adventurous, creative, and a dreamer.
+      Sometimes people make poor choices in their poor condition that result in
+      bad consequences, but others make positive choices that form positive
+      talents to last a lifetime.
+    </div>
+    <div className="about-us__text">
+      I remember as a young boy I asked my dad for a toy. He handed me an ax and
+      said, “With this tool, you can create any toy you can imagine.” As strange
+      as it sounds, he was right.
+    </div>
+    <div className="about-us__text">
+      I remember carrying that ax over my shoulder, looking at branches and
+      envisioning interesting pictures of toys to be carved. I began to expand
+      my imagination of what a toy should look like, and it enriched my
+      perceptions in creating ideas. I became adventurous and started to utilize
+      the environment around me for all my basic needs.
+    </div>
+    <div className="about-us__text">
+      There was a grocery store three miles away which we visited daily to buy
+      bread and milk. When I was old enough to make this trip, I began to see
+      cars and trucks that I had never imagined. I was fascinated by the beauty
+      and strangeness of them all. With each new vehicle I saw, I would run all
+      three miles home and share what I had seen.
+    </div>
+    <div className="about-us__text">
+      Because I was unable to describe the vehicles, I began carrying a piece of
+      scrap wire around my neck to make the outlined shape of the vehicle and
+      take it home to share. I began to add to the wire vehicle to the point
+      where I had created a completed vehicle out of trash. I was able to
+      “push-drive” them everywhere I went.
+    </div>
+    <div className="about-us__text">
+      I quickly acquired the nickname “Wire Boy” from those on the farm.
+    </div>
+    <div className="about-us__text">
+      My wire art blossomed when I came to America and allowed me to help fund
+      my schooling. Although I had plans to return to Zimbabwe afterwards, I
+      decided to use my passion of working with the youth to become a Youth
+      Minister in North Carolina. Although I love working with the youth in
+      America, I have never lost my passion for the work still to be done in
+      Zimbabwe.
     </div>
   </div>
 );
--- src/containers/about-us-container/about-us-container.js
+++ src/containers/about-us-container/about-us-container.js
@@ -1,5 +1,6 @@
 import React from "react";
 import VideoPlayer from "../../components/video-player";
+import AboutUs from "../../components/about-us";
 import "./about-us-container.scss";
 
 const AboutUsContainer = () => (
@@ -8,63 +9,7 @@ const AboutUsContainer = () => (
       <VideoPlayer src="https://www.youtube.com/embed/3jv-q_Nexgg" />
     </div>
     <div className="about-us-container__content">
-      <div className="about-us-container__content-title">About Me</div>
-      <div className="about-us-container__content-text">
-        My name is Jonathan Daniel. I was born in 1965 in Zimbabwe. I was born
-        and raised on a farm. Both of my parents were illiterate and are still
-        unable to read or write. My dad has been a cook for a white farmer for
-        over thirty years. He has two wives, and I am the oldest of his nine
-        children. Because my parents were so poor, I literally spent my
-        childhood with no clothes, money, or toys.
-      </div>
-      <div className="about-us-container__content-text">
-        Although we were materially poor, my family was rich in values and
-        tradition. Poverty forces you to be adventurous, creative and a dreamer.
-        Sometimes people make poor choices in their poor condition that result
-        in bad consequences, but others make positive choices that form positive
-        talents to last a lifetime. Allow me to explain:
-      </div>
-      <div className="about-us-container__content-text">
-        I remember as a young boy, when I asked my dad for a toy. He handed me
-        an ax and said, “With this tool, you can create any toy you can
-        imagine.” As strange as it sounds, he was right. I remember carrying
-        that ax over my shoulder, looking at branches and envisioning
-        interesting pictures of toys to be carved. I began expanding my
-        imagination of what a toy should look like, and I enriched my
-        perceptions in creating ideas. I became adventurous so that I started to
-        utilize the environment around me for all my basic needs.
-      </div>
-      <div className="about-us-container__content-text">
-        On the farm, I had often seen machinery such as a truck or a tractor,
-        but I hadn’t seen the other “animals of the road” when I was young.
-        There was a grocery store, merely three miles away from the farm, which
-        we visited each day to buy bread and milk. When I was old enough to make
-        this trip for the family’s only two food items, I began to see cars and
-        trucks that I had never imagined. I was fascinated by the beauty and
-        strangeness of them all. With each new vehicle I saw, I would run all
-        three miles home and share what I had seen. Because I was unable to
-        describe the kind of vehicle, I began carrying a piece of scrap wire
-        around my neck. That way, when I saw a car of interest, I could use the
-        wire to make the outlined shape of it and take it home to share. Next, I
-        began to add to the wire vehicle, to the point where I had created a
-        completed vehicle out of trash and was able to “push-drive” them
-        everywhere I went. I would even drive my trucks to the store and load
-        them up with the daily bread and milk items.
-      </div>
-      <div className="about-us-container__content-text">
-        I quickly acquired the nickname “Wire Boy” from those on the farm.
-      </div>
-      <div className="about-us-container__content-text">
-        The humble artistic beginnings of my wire art blossomed when I came to
-        the United States to study aircraft mechanics. The story of the
-        development of my craft and how it helped me to fund my schooling is an
-        interesting one as well. Although I had my own plans to return
-        immediately to Zimbabwe after schooling, I had to unselfishly decide to
-        use my gifts and talents of working with youth to be a Youth Minister in
-        North Carolina. Although I love working with youth in America, I have
-        never lost my passion for the work still to be done in my own country of
-        Zimbabwe.
-      </div>
+      <AboutUs />
     </div>
   </div>
 );
